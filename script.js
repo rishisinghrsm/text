a65cfb12105947d5fab99bd6f27901b6842cfa8b
Rishi Singh <rishisinghrsm@gmail.com>
2021-11-09 12:02:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const textarea = document.querySelector('textarea'),
+voicelist =document.querySelector('select'),
+speechBtn = document.querySelector('button');
+
+let synth = speechSynthesis,
+isspeaking = true;
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        // console.log(voice);
+        let selected = voice.name === " Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}> ${voice.name} (${voice.lang}) </option>`;
+        voicelist.insertAdjacentHTML('beforeend',option);
+    }
+}
+
+synth.addEventListener('voiceschanged',voices);
+
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voicelist.value){
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener('click', e => {
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+        }
+
+       if(textarea.value.length > 110){
+           if(isspeaking){
+               synth.resume();
+               isspeaking =false;
+            //    speechBtn.innerText = "Pause Speech";
+           }else{
+               synth.pause();
+               isspeaking =true;
+            //    speechBtn.innerText  = "Resume Speech";
+           }
+       }
+    
+    }
+})
